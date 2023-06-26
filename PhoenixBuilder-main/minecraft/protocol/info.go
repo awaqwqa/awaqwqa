cae1b120ab9970ca71b34f2ceebe7fa8cf9ebf8e
awaqwqa <614286773@qq.com>
2023-06-26 10:47:35 +0800

    删除以前无用项目

--- PhoenixBuilder-main/minecraft/protocol/info.go
+++
@@ -1,9 +0,0 @@
-package protocol
-
-const (
-	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 475
-	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.18.10"
-	// 475 -> 1.18.0.
-)
--- PhoenixBuilder-main/omega/third_party/yscore/define.go
+++
@@ -1,7 +0,0 @@
-package yscore
-
-const (
-	INTERFACE_FB_USERNAME = "fbname"
-)
-
-type STRING_FB_USERNAME string
